23a734d04d453e5e054fd9b15b2741e356c6ea41
softimi <softimideji@gmail.com>
2025-02-03 18:22:00 +0100

    Chapter 2
    Full Version of Chapter 2 | NO DB

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,11 +1,15 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Configuring Logger Service | ln 9 and 13
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers();
 
--- CompanyEmployees/obj/Debug/net6.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net6.0/CompanyEmployees.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdace27dbae7d43fa0daf34ad1ef727564108cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
