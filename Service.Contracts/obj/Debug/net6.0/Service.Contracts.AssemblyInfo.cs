c61e1df2a9a1a8a9cb183067a84f0cf5f5ccea77
softimi <softimideji@gmail.com>
2026-04-24 06:46:59 +0100

    Chapter 10
    Full Version of Chapter 10 | DB

--- Service.Contracts/obj/Debug/net6.0/Service.Contracts.AssemblyInfo.cs
+++ Service.Contracts/obj/Debug/net6.0/Service.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05ce1523e73e6d7aace722ccaa1947fcd07f0a2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020e2faea764f02ce0eb3a7b67941c8570555c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
